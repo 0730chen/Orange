e35312e8fec14b1dfd26b913df477d2d22275390
0730chen <1252331215@qq.com>
2020-02-24 12:27:54 +0800

    deploy

---
+++ precache-manifest.1886544b0e6a0549b15062e7f40cf456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5114ca2898a254a94264",
+    "url": "/Orange/css/app.806a9258.css"
+  },
+  {
+    "revision": "0176362fb6ea5a342a899310a1893958",
+    "url": "/Orange/index.html"
+  },
+  {
+    "revision": "5114ca2898a254a94264",
+    "url": "/Orange/js/app.0252227a.js"
+  },
+  {
+    "revision": "4f5e8a717d227fd29dbd",
+    "url": "/Orange/js/chunk-vendors.785c7731.js"
+  },
+  {
+    "revision": "b0ed07290d564550e2ef10d2f756d908",
+    "url": "/Orange/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Orange/robots.txt"
+  }
+]);
